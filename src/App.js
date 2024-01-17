c494f0fa04b60c69a4a43d7b8d7d35dd38d20b50
gk124 <gunjanku109@gmail.com>
2024-01-17 17:34:22 +0530

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
 function App() {
+
+  const getRow = (row) => {
+    let res = [1];
+    let temp = [];
+    let series = [res.slice()];
+
+    if (row === 1) {
+      return series;
+    }
+
+    res.push(1);
+
+    series.push(res.slice());
+    if (row === 2) {
+      return series;
+    }
+
+
+    for (let i = 3; i <= row; i++) {
+      temp.push(1);
+      for (let j = 1; j < res.length; j++) {
+        temp.push(res[j] + res[j - 1]);
+      }
+      temp.push(1);
+      res = temp.slice();
+      series.push(res.slice());
+      temp = [];
+    }
+    return series;
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{textAlign:"center",marginTop: '20vh'}}>
+      <h3>Pascal's Triangle</h3>
+      {getRow(5).map((row, index) => (
+        <div key={index}>
+            {Array.from({ length: 5 - index - 1 }, (_, i) => (
+              <span key={i}>&nbsp;</span>
+          ))}
+          {row.map((val,ind)=>(
+            <span key={ind}>{val+' '}&nbsp;</span>
+          ))}
+          {Array.from({ length: 5 - index - 1 }, (_, i) => (
+              <span key={i}>&nbsp;</span>
+          ))}
+        </div>
+      ))}
     </div>
   );
 }
